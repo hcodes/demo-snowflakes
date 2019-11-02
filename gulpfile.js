c23ac05f090b562870e969706f20afd941c626b3
Denis Seleznev <hcodes@yandex.ru>
2019-11-02 14:00:50 +0300

    Upgrade dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@
 const
     gulp = require('gulp'),
     $ = require('gulp-load-plugins')(),
+    alias = require('@rollup/plugin-alias'),
     babel = require('rollup-plugin-babel'),
-    alias = require('rollup-plugin-alias'),
     replace = require('rollup-plugin-replace');
 
 gulp.task('js', function js() {
@@ -14,7 +14,9 @@ gulp.task('js', function js() {
             output: { format: 'iife' },
             plugins: [
                 alias({
-                    'vue': 'node_modules/vue/dist/vue.esm.js'
+                    entries: {
+                        'vue': 'node_modules/vue/dist/vue.esm.js'
+                    }
                 }),
                 replace({
                     'process.env.NODE_ENV': JSON.stringify('production')
@@ -30,9 +32,7 @@ gulp.task('css', function css() {
     return gulp.src('src/index.less')
         .pipe($.less())
         .pipe($.cleancss())
-        .pipe($.autoprefixer({
-            browsers: ['ie >= 10', 'Firefox >= 24', 'Chrome >= 26', 'iOS >= 6', 'Safari >= 6', 'Android > 4.0']
-        }))
+        .pipe($.autoprefixer())
         .pipe(gulp.dest('dist/'));
 });
 
